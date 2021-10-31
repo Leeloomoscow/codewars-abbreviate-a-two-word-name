503a95e50ac48d7e928242498b22aadcd81de057
Aleksandra Rudenko <rudenkoay8@gmail.com>
2021-10-31 22:00:33 +0300

    Done

--- src/test/java/MyTest.java
+++ src/test/java/MyTest.java
@@ -1,8 +1,21 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MyTest {
     @Test
-    void name() {
+    public void testFixed() {
+    assertEquals("S.H", AbbreviateTwoWords.abbrevName("Sam Harris"));
+    assertEquals("P.F", AbbreviateTwoWords.abbrevName("Patrick Feenan"));
+    assertEquals("E.C", AbbreviateTwoWords.abbrevName("Evan Cole"));
+    assertEquals("P.F", AbbreviateTwoWords.abbrevName("P Favuzzi"));
+    assertEquals("D.M", AbbreviateTwoWords.abbrevName("David Mendieta"));
+}
 
+    private static class AbbreviateTwoWords {
+        public static String abbrevName(String name) {
+            return name.charAt(0) + "." + name.charAt(name.indexOf(" ") + 1);
+        }
     }
 }
+
